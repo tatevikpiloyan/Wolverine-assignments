65ee9c67549d568b235bab025d464a65077a190a
Tatev <tatev@Tatevs-MacBook-Pro.local>
2022-09-22 19:39:12 +0400

    Update

---
+++ sep-19-2022/map_calc.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <map>
+
+int my_sum(int, int);
+int my_sub(int, int);
+int my_mul(int, int);
+int my_div(int, int);
+
+int main()
+{
+	char ch;
+	std::cout << "Please enter arithmetic operation!" << std::endl;
+	std::cin >> ch;
+	int num1;
+	int num2;
+	std::cout << "Please enter two numbers to operate!" << std::endl;
+	std::cin >> num1 >> num2;
+	std::map<char, int(*)(int, int)> result;
+	result.insert(std::make_pair('+', my_sum));
+	result.insert(std::make_pair('-', my_sub));
+	result.insert(std::make_pair('*', my_mul));
+	result.insert(std::make_pair('/', my_div));
+	if (auto key = result.find(ch); key != result.end())
+	{
+		std::cout << key->second(num1, num2) << std::endl;
+	}
+	else if (auto key = result.find(ch); key != result.end())
+	{
+		std::cout << key->second(num1, num2) << std::endl;
+	}
+	else if (auto key = result.find(ch); key != result.end())
+    {
+		std::cout << key->second(num1, num2) << std::endl;
+    }
+	else if (auto key = result.find(ch); key != result.end())
+    {
+		std::cout << key->second(num1, num2) << std::endl;
+    }
+	else
+	{
+		std::cout << "Wrong operation!" << std::endl;
+	}
+}
+
+int my_sum(int a, int b)
+{
+	return a + b;
+}
+
+int my_sub(int a, int b)
+{
+	return a - b;
+}
+
+int my_mul(int a, int b)
+{
+	return a * b;
+}
+
+int my_div(int a, int b)
+{
+	return a / b;
+}
