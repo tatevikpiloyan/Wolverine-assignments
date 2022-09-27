f322d6390135b6ff12ab04e9289d13e50134f3fe
Tatev <tatev@Tatevs-MacBook-Pro.local>
2022-09-27 22:42:00 +0400

    Update

---
+++ sep-27-2022/split.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+std::vector<std::string> split(const std::string&, std::string&);
+
+int main()
+{
+	std::string str;
+	std::string spliter;
+	std::cout << "Please enter a string!" << std::endl;
+	getline(std::cin, str);
+	std::cout << "Please enter spliters!" << std::endl;
+	getline(std::cin, spliter);
+	std::vector<std::string> vec;
+	vec  = split(str, spliter);
+	for (auto& i : vec)
+	{
+		std::cout << i << ' ' << std::endl;
+	}
+}
+
+std::vector <std::string> split(const std::string& str, std::string& spliter)
+{
+	std::vector<std::string> nstr;
+	std::string tstr;
+	for (int i = 0; i < str.size(); ++i)
+	{
+		if (spliter.find(str[i]) != -1)
+		{
+			nstr.push_back(tstr);
+			tstr = {};
+			continue;
+		}
+		tstr.push_back(str[i]);
+	}
+	nstr.push_back(tstr);
+	return nstr;
+}
